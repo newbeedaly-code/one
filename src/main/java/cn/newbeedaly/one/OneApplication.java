8f3fec5387003985f3235c357d03236b883e7974
李乾坤 <newbeedaly@163.com>
2023-02-09 20:06:51 +0800

    complete snapshot version

--- src/main/java/cn/newbeedaly/one/OneApplication.java
+++ src/main/java/cn/newbeedaly/one/OneApplication.java
@@ -2,12 +2,17 @@ package cn.newbeedaly.one;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableEurekaClient
+@EnableFeignClients(basePackages = "cn.newbeedaly")
 @ComponentScan(basePackages = "cn.newbeedaly")
 @SpringBootApplication
 public class OneApplication {
 
+    // todo 添加BaseController 参考 https://blog.csdn.net/weixin_43641119/article/details/111658469
     public static void main(String[] args) {
         SpringApplication.run(OneApplication.class, args);
     }
